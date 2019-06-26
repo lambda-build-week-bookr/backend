611a5cc93a75664bce7ede74031cb78f3da7fde9
Arron M <marshall.arron@gmail.com>
2019-06-25 23:20:53 -0700

    Update cleanup seed for postgres

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -2,7 +2,6 @@ const cleaner = require('knex-cleaner');
 
 exports.seed = knex => {
   return cleaner.clean(knex, {
-    mode: 'truncate',
     ignoreTables: ['knex_migrations', 'knex_migrations_lock']
   });
 };
